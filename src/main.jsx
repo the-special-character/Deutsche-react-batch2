1cac496a99fd99a1c5783b9fde629bd20f4cbfcb
Yagnesh Modh <contact@thespecialcharacter.com>
2024-07-30 09:38:23 +0530

    commit code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./components/customInput";
+import Counter from "./components/counter";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>Hello world</h1>
+    <input type="password" />
+    <App label="Name" id="name" />
+    <App label="Email" type="email" id="email" />
+    <App label="Password" type="password" id="password" />
+    <App label="Confirm Password" type="password" id="confirmPassword" />
+    <Counter />
+  </div>
+);
