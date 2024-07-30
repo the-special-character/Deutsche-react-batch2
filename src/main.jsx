ab01d240e895a3759e0eed53f266cc39c20a1e15
Yagnesh Modh <contact@thespecialcharacter.com>
2024-07-30 10:07:55 +0530

    clearn todo branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,3 @@
 import ReactDOM from "react-dom/client";
-import App from "./components/customInput";
-import Counter from "./components/counter";
-import Test from "./components/test";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>
-    <h1>Hello world</h1>
-    <input type="password" />
-    <App label="Name" id="name" />
-    <App label="Email" type="email" id="email" />
-    <App label="Password" type="password" id="password" />
-    <App label="Confirm Password" type="password" id="confirmPassword" />
-    <Counter />
-    <Test name="yagnesh" />
-  </div>
-);
+ReactDOM.createRoot(document.getElementById("root")).render();
